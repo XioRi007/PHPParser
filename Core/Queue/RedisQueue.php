fd583788179238e9ca29ce91d30f4a4eaa09419b
XioRi007 <mariyaivanova@ukr.net>
2023-05-15 11:38:13 +0300

    fixing proxy watcher and adding debug to queu

--- Core/Queue/RedisQueue.php
+++ Core/Queue/RedisQueue.php
@@ -61,15 +61,14 @@ class RedisQueue implements IQueue
         foreach (new Iterator\Keyspace($this->client, $this->queueName . ":*", 1) as $tmp) {
             $key = $tmp;
             if ($key == "") {
-                $this->logger->info("Message is empty");
+                $this->logger->debug("Message is empty");
                 continue;
             }
             $clearKey = substr($key, strrpos($key, ':') + 1);
             $dest = $this->hiddenQueueName . ":" . $clearKey;
             $res = $this->client->rpoplpush($key, $dest);
             if ($res == "" || !is_string($res)) {
-                $this->logger->info("Message has already been taken");
-                continue;
+                $this->logger->debug("Message has already been taken");
             } else {
                 break;
             }
